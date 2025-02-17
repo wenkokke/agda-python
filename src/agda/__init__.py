ad6abfb1dfed2ad8b2111328592dd7231ecd20e9
Wen Kokke <wenkokke@users.noreply.github.com>
2025-02-17 12:33:27 +0000

    feat: safer rts usage

--- src/agda/__init__.py
+++ src/agda/__init__.py
@@ -1,4 +1,7 @@
-from typing import List
+import atexit
+from contextlib import contextmanager
+from threading import Lock
+from typing import Iterator, List
 
 from ._binding import (
     unsafe_hs_agda_version,
@@ -9,18 +12,29 @@ from ._binding import (
 
 VERSION: str = "2.7.0"
 
+_hs_rts_init: bool = False
+_hs_rts_lock: Lock = Lock()
+
+def hs_rts_exit() -> None:
+    global _hs_rts_lock
+    with _hs_rts_lock:
+        unsafe_hs_exit()
+
+@contextmanager
+def hs_rts_init(args: List[str] = []) -> Iterator[None]:
+    global _hs_rts_init
+    global _hs_rts_lock
+    with _hs_rts_lock:
+        if not _hs_rts_init:
+            _hs_rts_init = True
+            unsafe_hs_init(args)
+            atexit.register(hs_rts_exit)
+    yield None
 
 def version() -> str:
-    try:
-        unsafe_hs_init([])
-        return str(unsafe_hs_agda_version())
-    finally:
-        unsafe_hs_exit()
+    with hs_rts_init():
+        return unsafe_hs_agda_version()
 
-
-def main(args: List[str]) -> int:
-    try:
-        unsafe_hs_init(args)
-        return unsafe_hs_agda_main()
-    finally:
-        unsafe_hs_exit()
+def main(args: List[str] = []) -> int:
+    with hs_rts_init(args):
+        return unsafe_hs_exit()
